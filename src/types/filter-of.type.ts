69d4a609a4a115a82b4abbb567d9bfdf6398e85d
Gabriel Silva <gabrieljsilva2301@gmail.com>
2024-02-07 20:15:38 -0300

    fix: type FilterOf to be completly partial

--- src/types/filter-of.type.ts
+++ src/types/filter-of.type.ts
@@ -1,7 +1,7 @@
-import { BooleanFilter } from '../filters/boolean-filter.filter';
-import { FloatFilter } from '../filters/float-filter.filter';
-import { StringFilter } from '../filters/string.filter';
-import { DateTimeFilter } from '../filters/date-time.filter';
+import { BooleanFilter } from "../filters/boolean-filter.filter";
+import { FloatFilter } from "../filters/float-filter.filter";
+import { StringFilter } from "../filters/string.filter";
+import { DateTimeFilter } from "../filters/date-time.filter";
 
 type ArrayFilter<T> = T extends Array<infer U> ? FilterOf<U> : never;
 
@@ -25,4 +25,4 @@ export type LogicalOperationsFilter<T> = {
   _NOT: FilterOf<T>;
 };
 
-export type FilterOf<T> = BaseFilter<T> & LogicalOperationsFilter<T>;
+export type FilterOf<T> = Partial<BaseFilter<T> & LogicalOperationsFilter<T>>;
